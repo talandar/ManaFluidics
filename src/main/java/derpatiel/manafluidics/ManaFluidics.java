783a6ed2ce481f076980e4835660a89e3488db0d
Talandar <jmtalandar@gmail.com>
2016-09-18 23:28:14 -0400

    initial checkin, mod file, etc

---
+++ src/main/java/derpatiel/manafluidics/ManaFluidics.java
@@ -0,0 +1,18 @@
+package derpatiel.manafluidics;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid =  ManaFluidics.MODID, version =  ManaFluidics.VERSION)
+public class ManaFluidics
+{
+    public static final String MODID = "manafluidics";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    }
+}
