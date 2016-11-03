64d0a129d096c31a9c82e395e3943d199ae44148
Talandar <jmtalandar@gmail.com>
2016-11-03 11:16:54 -0400

    a bit of understanding work on commands

--- src/main/java/derpatiel/manafluidics/command/MFCommand.java
+++ src/main/java/derpatiel/manafluidics/command/MFCommand.java
@@ -1,12 +1,10 @@
 package derpatiel.manafluidics.command;
 
-import com.google.common.collect.Lists;
-import derpatiel.manafluidics.util.ChatUtil;
 import derpatiel.manafluidics.util.LOG;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
-import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 
@@ -22,12 +20,28 @@ public class MFCommand extends CommandBase{
 
     @Override
     public String getCommandUsage(ICommandSender sender) {
-        return "manafluidics <command>";
+        return "manafluidics <username> <command> or manafluidics help";
     }
 
     @Override
     public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-        LOG.info("command!");
+        EntityPlayer player = (EntityPlayer)sender.getCommandSenderEntity();
+        LOG.info("Sender: "+player.getName());
+        //self: a command like /manafluidics help has 'help' as args[0].  no args if no text past manafluidics
+    }
 
+    @Override
+    public int getRequiredPermissionLevel() {
+        return super.getRequiredPermissionLevel();
+    }
+
+    @Override
+    public List<String> getTabCompletionOptions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos pos) {
+        return super.getTabCompletionOptions(server, sender, args, pos);
+    }
+
+    @Override
+    public boolean isUsernameIndex(String[] args, int index) {
+        return super.isUsernameIndex(args, index);
     }
 }
